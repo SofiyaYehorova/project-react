54015eb7f4f49ee54ab8b1e76ee08794e0060cdc
SofiyaYehorova <yehorova66@gmail.com>
2022-09-16 16:56:57 +0300

    hw1-part2

--- src/components/description/Character.js
+++ src/components/description/Character.js
@@ -1,9 +1,11 @@
-function Character ({person}){
+function Character (props){
 
+    let {person}=props;
     return(
         <div>
             <h2>{person.id} -- {person.name}</h2>
             <p>{person.gender}</p>
+
 
         </div>
     )
